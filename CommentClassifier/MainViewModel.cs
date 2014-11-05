2753cca0eee04c938a75c7af5ea915ffe67fbf35
Samuel Carlsson <samuel.carlsson@volumental.com>
2014-11-05 08:45:54 +0100

    Now possible to load and save comment classification

--- CommentClassifier/MainViewModel.cs
+++ CommentClassifier/MainViewModel.cs
@@ -7,33 +7,96 @@ namespace CommentClassifier
 {
     class SourceCodeFile
     {
+        private readonly FileInfo _file;
         private readonly Dictionary<int, string> _commentCategories;
 
-        public SourceCodeFile(List<int> commentLines)
+        public SourceCodeFile(FileInfo file, Dictionary<int, string> commentLines)
         {
-            _commentCategories = new Dictionary<int, string>(commentLines.Count);
-            foreach (var commentLine in commentLines)
-                _commentCategories.Add(commentLine, null);
+            _file = file;
+            _commentCategories = commentLines;
         }
+
+        public Dictionary<int, string> Categories { get { return _commentCategories; } }
+
+        public FileInfo File { get { return _file; } }
     }
 
     public class MainViewModel: ViewModel
     {
-        private string _path;
+        private string _folderPath;
 
         private List<SourceCodeFile> _sourceCode;
         
-        public string Path
+        public string FolderPath
         {
-            get { return _path; }
+            get { return _folderPath; }
             set
             {
-                _path = value;
+                _folderPath = value;
                 RaisePropertyChanged();
             }
         }
 
         public ICommand Scan { get { return new DelegatingCommand(DoScan); } }
+
+        public ICommand Save { get { return new DelegatingCommand(DoSave); } }
+
+        public ICommand Load { get { return new DelegatingCommand(DoLoad); } }
+
+        private void DoSave()
+        {
+            var file = new FileInfo(Path.Combine(FolderPath, "comments.txt"));
+            using (var writer = new StreamWriter(file.OpenWrite()))
+            {
+                writer.WriteLine("# comment types");
+                foreach (var sourceCodeFile in _sourceCode)
+                {
+                    writer.WriteLine("{0}", sourceCodeFile.File);
+
+                    foreach (var entry in sourceCodeFile.Categories)
+                    {
+                        var lineNumber = entry.Key;
+                        var category = entry.Value;
+                        writer.WriteLine("{0}={1}", lineNumber, category);
+                    }
+                }
+            }
+        }
+
+        private void DoLoad()
+        {
+            var file = new FileInfo(Path.Combine(FolderPath, "comments.txt"));
+            var result = new List<SourceCodeFile>();
+            using (var reader = file.OpenText())
+            {
+                string filename = null;
+                var dict = new Dictionary<int, string>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    if (!line.StartsWith("#"))
+                    {
+                        var parts = line.Split('=');
+                        if (parts.Length == 1)
+                        {
+                            if (filename != null)
+                            {
+                                result.Add(new SourceCodeFile(new FileInfo(filename), dict));
+                            }
+                            filename = line;
+                        }
+                        if (parts.Length == 2)
+                        {
+                            dict[int.Parse(parts[0])] = parts[1];
+                        }
+                    }
+                }
+                if (filename != null)
+                {
+                    result.Add(new SourceCodeFile(new FileInfo(filename), dict));
+                }
+            }
+        }
 
         private IEnumerable<FileInfo> Search(DirectoryInfo root, string[] extensions)
         {
@@ -53,7 +116,7 @@ namespace CommentClassifier
         private void DoScan()
         {
             var sourceCode = new List<SourceCodeFile>();
-            foreach (var f in Search(new DirectoryInfo(Path), new[] { ".cpp", ".hpp", ".h" }))
+            foreach (var f in Search(new DirectoryInfo(_folderPath), new[] { ".cpp", ".hpp", ".h" }))
             {
                 var comments = new List<int>();
                 bool inComment = false;
@@ -75,7 +138,12 @@ namespace CommentClassifier
                         lineNumber++;
                     }
                 }
-                sourceCode.Add(new SourceCodeFile(comments));
+
+                var tmp = new Dictionary<int, string>(comments.Count);
+                foreach (var commentLine in comments)
+                    tmp.Add(commentLine, null);
+
+                sourceCode.Add(new SourceCodeFile(f, tmp));
 
             }
             _sourceCode = sourceCode;
